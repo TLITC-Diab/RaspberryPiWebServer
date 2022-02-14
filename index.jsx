d3c20e232f7d6885d0b620a0136f8f206b57b9fb
Diab <59467981+TLITC-Diab@users.noreply.github.com>
2022-02-13 21:53:19 -0500

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default class extends React.Component {
   static async getInitialProps() {
-    const jsonFile = require('./num_got_lucky.json');
   }
 
   render() {
+    const jsonFile = require('./num_got_lucky.json');
     return jsonFile;
     return <button>Push If You Just Had Sex</button>
   }
