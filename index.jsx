d8fdb127cbf0c0d35c414eef378477e2cf666aa1
Diab <59467981+TLITC-Diab@users.noreply.github.com>
2022-02-13 21:43:11 -0500

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -2,24 +2,12 @@ import React from "react";
 
 export default class extends React.Component {
   static async getInitialProps() {
-    const fs = require('fs');
-
-    fs.readFile('./num_got_lucky.json', 'utf8', (err, data) => {
-
-      if (err) {
-        console.log(`Error reading file from disk: ${err}`);
-      } else {
-
-        // parse JSON string to JSON object
-        const num_got_lucky = JSON.parse(data);
-
-        });
-      }
-
-    });
-
-  render(); {
-    return <p></p>;
-    return <button>"Click If You Just Had Sex"</button>;
+    var json = await fetch("/time").then(resp => resp.json());
+    return { time: json.time };
+    const jsonFile = require('./num_got_lucky.json');
   }
-}}
+
+  render() {
+    return <p>jsonFile</p>;
+  }
+}
